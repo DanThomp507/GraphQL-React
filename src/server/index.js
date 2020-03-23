ec00026bf55eaa8c557de00f5131410f53d3e799
Daniel Thompson <DanThomp507@gmail.com>
2020-03-23 15:41:08 -0400

    set up simple server

---
+++ src/server/index.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const cors = require("cors")
+const graphqlHTTP = require("express-graphql")
+const gql = require("graphql-tag")
+const { buildASTSchema } = require("graphql")
+
+const POSTS = [
+  {
+    author: "Daniel Thompson",
+    body: "Initial post"
+  },
+  {
+    author: "Andrew Thompson",
+    body: "I am here"
+  }
+]
+
+const schema = buildASTSchema(gql`
+  type Query {
+    posts: [Post]
+    post(id: ID!): Post
+  }
+
+  type Post {
+    id: ID
+    author: String
+    body: String
+  }
+`)
+
+const mapPost = (post, id) => post && { id, ...post }
+
+const root = {
+  posts: () => POSTS.map(mapPost),
+  post: ({ id }) => mapPost(POSTS[id], id)
+}
+
+const app = express()
+app.use(cors())
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  })
+)
+
+const port = process.env.PORT || 4000
+app.listen(port)
+console.log(`Running server at localhost:${port}/graphql`)

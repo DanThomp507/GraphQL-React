651062e309aeea82872400f2d4ad8d4e787c4277
Daniel Thompson <DanThomp507@gmail.com>
2020-03-23 15:49:28 -0400

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { ApolloProvider } from 'react-apollo'
+import * as serviceWorker from './serviceWorker'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import client from './apollo'
+
+ReactDOM.render(<App />, document.getElementById('root'))
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
+serviceWorker.unregister()
+if (module.hot) module.hot.accept()
